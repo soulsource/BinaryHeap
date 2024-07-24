76e6405c9f810bf9830756bb20ad9e4be3214b89
Andreas Grois <andi@grois.info>
2024-07-24 14:27:43 +0200

    Fix performance issue in CompleteTree.contains.

--- BinaryHeap/CompleteTree/AdditionalOperations.lean
+++ BinaryHeap/CompleteTree/AdditionalOperations.lean
@@ -67,13 +67,14 @@ instance {α : Type u} [DecidableEq α] {tree : CompleteTree α n} {element : α
         Decidable.isTrue $ Or.inl t
       else
         have hl := go p l
-        have hr := go q r
         if h₁ : hl.decide then
           Decidable.isTrue $ Or.inr $ Or.inl $ decide_eq_true_eq.mp h₁
-        else if h₂ : hr.decide then
-          Decidable.isTrue $ Or.inr $ Or.inr $ decide_eq_true_eq.mp h₂
         else
-          have h₁ : ¬l.contains element := decide_eq_true_eq.subst h₁
-          have h₂ : ¬r.contains element := decide_eq_true_eq.subst h₂
-          Decidable.isFalse $ not_or_intro t $ not_or_intro h₁ h₂
+          have hr := go q r
+          if h₂ : hr.decide then
+            Decidable.isTrue $ Or.inr $ Or.inr $ decide_eq_true_eq.mp h₂
+          else
+            have h₁ : ¬l.contains element := decide_eq_true_eq.subst h₁
+            have h₂ : ¬r.contains element := decide_eq_true_eq.subst h₂
+            Decidable.isFalse $ not_or_intro t $ not_or_intro h₁ h₂
   go n tree
